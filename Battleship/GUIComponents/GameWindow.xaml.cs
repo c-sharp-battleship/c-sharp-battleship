3931b54e625fce7238fda973fb8e6079b9186083
Samuel Mace <samuelcmace@gmail.com>
2022-04-15 10:20:35 -0500

    Fix StyleCop Errors

--- Battleship/GUIComponents/GameWindow.xaml.cs
+++ Battleship/GUIComponents/GameWindow.xaml.cs
@@ -1,25 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="GameWindow.xaml.cs" company="Team">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace Battleship.GUIComponents
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
     /// <summary>
     /// Interaction logic for GameWindow.xaml
     /// </summary>
     public partial class GameWindow : Window
     {
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="GameWindow" /> class.
+        /// </summary>
         public GameWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
