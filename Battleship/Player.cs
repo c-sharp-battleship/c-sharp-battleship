3abfd37d60fd3d747c59ccc1a7fbaf580ce7d69b
Gsorto86 <103615507+Gsorto86@users.noreply.github.com>
2022-04-25 18:34:38 -0500

    Update Player.cs
    Removed ship background

--- Battleship/Player.cs
+++ Battleship/Player.cs
@@ -114,14 +114,17 @@ namespace Battleship
             for (int i = 1; i <= 5; i++)
             {
                 //get an iamge retainer for the iteration
+                /*
                 BitmapImage ShipPic = new BitmapImage();
                 ShipPic.BeginInit();
                 ShipPic.UriSource = new Uri(@"DinamicImages/Warship.jpg", UriKind.RelativeOrAbsolute);//Load dinamic image
                 ShipPic.EndInit();
+                */
 
                 //Construct the ships with the image above
                 Ship Warship = new Ship(Player_ID,"Ship"+i.ToString(), 3, 0,3, gridcellSize);
-                Warship.Background = new ImageBrush(ShipPic);
+                //Warship.Background = new ImageBrush(ShipPic);
+                Warship.Background = Brushes.Yellow;
                 Warship.Uid = i.ToString();
                 Canvas.SetTop(Warship, i * gridcellSize);
                 Canvas.SetLeft(Warship,0);
