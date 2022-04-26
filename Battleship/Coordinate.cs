e2326f292fb8e8bb38a1dff44ba3043a0b57440b
Samuel Mace <samuelcmace@gmail.com>
2022-04-26 16:51:00 -0500

    Add XCoordinateLetter Getter to Coordinate Class

--- Battleship/Coordinate.cs
+++ Battleship/Coordinate.cs
@@ -16,6 +16,49 @@ namespace Battleship
         /// </summary>
         public short YCoordinate;
 
+        public char XCoordinateLetter
+        {
+            get
+            {
+                switch (this.XCoordinate)
+                {
+                    case 1:
+                        return 'A';
+                        break;
+                    case 2:
+                        return 'B';
+                        break;
+                    case 3:
+                        return 'C';
+                        break;
+                    case 4:
+                        return 'D';
+                        break;
+                    case 5:
+                        return 'E';
+                        break;
+                    case 6:
+                        return 'F';
+                        break;
+                    case 7:
+                        return 'G';
+                        break;
+                    case 8:
+                        return 'H';
+                        break;
+                    case 9:
+                        return 'I';
+                        break;
+                    case 10:
+                        return 'J';
+                        break;
+                    default:
+                        return 'Z';
+                        break;
+                }
+            }
+        }
+
         /// <summary>
         ///  Initializes a new instance of the <see cref="Coordinate" /> class.
         /// </summary>
