094072470607db2f268168ff47a783468205d727
Gsorto86 <103615507+Gsorto86@users.noreply.github.com>
2022-05-12 19:03:32 -0500

    Ship selection frame

--- Battleship/AdvancedOptionsWindow.xaml.cs
+++ Battleship/AdvancedOptionsWindow.xaml.cs
@@ -20,6 +20,11 @@ namespace Battleship
         private AdvancedOptions advancedOptions;
 
         /// <summary>
+        /// The ship loading window.
+        /// </summary>
+        private Ship_Loading shipLoading;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="AdvancedOptionsWindow"/> class.
         /// </summary>
         /// <param name="p_advancedOptions">The advancedOptions instance to be passed by reference and assigned as a class-member object.</param>
@@ -28,6 +33,7 @@ namespace Battleship
             this.InitializeComponent();
 
             this.advancedOptions = p_advancedOptions;
+            this.shipLoading = new Ship_Loading(ref p_advancedOptions);
         }
 
         /// <summary>
@@ -38,22 +44,6 @@ namespace Battleship
         /// <exception cref="Exception">Thrown if the <see cref="advancedOptions"/>'s FleetSize is invalid.</exception>
         private void AdvancedOptionsWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            switch (this.advancedOptions.FleetSize)
-            {
-                case 3:
-                    this.AdjustableFleetSize3RadioButton.IsChecked = true;
-                    break;
-                case 5:
-                    this.AdjustableFleetSize5RadioButton.IsChecked = true;
-                    break;
-                case 7:
-                    this.AdjustableFleetSize5RadioButton.IsChecked = true;
-                    break;
-                default:
-                    throw new Exception();
-                    break;
-            }
-
             switch (this.advancedOptions.GridSize)
             {
                 case 8:
@@ -69,134 +59,6 @@ namespace Battleship
                     throw new Exception();
                     break;
             }
-        }
-
-        /// <summary>
-        /// Method to set the <see cref="ComboBox"/> IsEnabled property.
-        /// </summary>
-        /// <param name="ship1">Whether or not the first ComboBox should be enabled.</param>
-        /// <param name="ship2">Whether or not the second ComboBox should be enabled.</param>
-        /// <param name="ship3">Whether or not the third ComboBox should be enabled.</param>
-        /// <param name="ship4">Whether or not the fourth ComboBox should be enabled.</param>
-        /// <param name="ship5">Whether or not the fifth ComboBox should be enabled.</param>
-        /// <param name="ship6">Whether or not the sixth ComboBox should be enabled.</param>
-        /// <param name="ship7">Whether or not the seventh ComboBox should be enabled.</param>
-        private void SetFleetSizeComboBoxDisability(bool ship1, bool ship2, bool ship3, bool ship4, bool ship5, bool ship6, bool ship7)
-        {
-            // Set the Default Values for Adjustable Fleet Size
-            this.AdjustableFleetSize1ComboBox.IsEnabled = ship1;
-            this.AdjustableFleetSize2ComboBox.IsEnabled = ship2;
-            this.AdjustableFleetSize3ComboBox.IsEnabled = ship3;
-            this.AdjustableFleetSize4ComboBox.IsEnabled = ship4;
-            this.AdjustableFleetSize5ComboBox.IsEnabled = ship5;
-            this.AdjustableFleetSize6ComboBox.IsEnabled = ship6;
-            this.AdjustableFleetSize7ComboBox.IsEnabled = ship7;
-        }
-
-        /// <summary>
-        /// Checked event for the <see cref="AdjustableFleetSize3RadioButton"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize3RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            this.advancedOptions.FleetSize = 3;
-
-            this.SetFleetSizeComboBoxDisability(true, true, true, false, false, false, false);
-        }
-
-        /// <summary>
-        /// Checked event for the <see cref="AdjustableFleetSize5RadioButton"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize5RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            this.advancedOptions.FleetSize = 5;
-
-            this.SetFleetSizeComboBoxDisability(true, true, true, true, true, false, false);
-        }
-
-        /// <summary>
-        /// Checked event for the <see cref="AdjustableFleetSize7RadioButton"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize7RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            this.advancedOptions.FleetSize = 7;
-
-            this.SetFleetSizeComboBoxDisability(true, true, true, true, true, true, true);
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize1ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize1ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize1ComboBox_SelectionChanged
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize2ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize2ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize2ComboBox_SelectionChanged
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize3ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize3ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize3ComboBox_SelectionChanged
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize4ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize4ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize4ComboBox_SelectionChanged
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize5ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize5ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize5ComboBox_SelectionChanged
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize6ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize6ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize6ComboBox_SelectionChanged
-        }
-
-        /// <summary>
-        /// SelectionChanged event for the <see cref="AdjustableFleetSize7ComboBox"/>.
-        /// </summary>
-        /// <param name="sender">The sender that invoked the event.</param>
-        /// <param name="e">The arguments passed to the event.</param>
-        private void AdjustableFleetSize7ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // TODO: Implement AdjustableFleetSize7ComboBox_SelectionChanged
         }
 
         /// <summary>
@@ -285,7 +147,6 @@ namespace Battleship
         /// <param name="e">The arguments passed to the event.</param>
         private void DefaultOptionsButton_Click(object sender, RoutedEventArgs e)
         {
-            this.AdjustableFleetSize5RadioButton.IsChecked = true;
             this.AdjustableGridSize10RadioButton.IsChecked = true;
             this.MultipleAttacksPerSuccessfulAttackCheckBox.IsChecked = false;
             this.MultipleAttacksPerTurnPerShip.IsChecked = false;
@@ -300,6 +161,11 @@ namespace Battleship
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void CustomFleetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.shipLoading.Show();
         }
     }
 }
---
+++ Battleship/Ship_Loading.xaml.cs
@@ -0,0 +1,157 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Ship_Loading.xaml.cs" company="Battleship Coding Group">
+//     Battleship Coding Group, 2022
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Battleship
+{
+    using System.Collections.Generic;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for Ship_Loading.xaml.
+    /// </summary>
+    public partial class Ship_Loading : Window
+    {
+        private AdvancedOptions advancedOptions;
+
+        public Ship_Loading(ref AdvancedOptions p_advancedOptions)
+        {
+            this.InitializeComponent();
+
+            this.advancedOptions = p_advancedOptions;
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(StatusCodes.ShipType ship in this.advancedOptions.ShipTypes)
+            {
+                switch(ship)
+                {
+                    case StatusCodes.ShipType.DESTROYER:
+                        this.RPT_LIST.Items.Add("Destroyer");
+                        break;
+                    case StatusCodes.ShipType.SUBMARINE:
+                        this.RPT_LIST.Items.Add("Submarine");
+                        break;
+                    case StatusCodes.ShipType.CRUISER:
+                        this.RPT_LIST.Items.Add("Cruiser");
+                        break;
+                    case StatusCodes.ShipType.BATTLESHIP:
+                        this.RPT_LIST.Items.Add("Battleship");
+                        break;
+                    case StatusCodes.ShipType.CARRIER:
+                        this.RPT_LIST.Items.Add("Carrier");
+                        break;
+                }
+            }
+        }
+
+        private void DE_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.advancedOptions.ShipTypes.Count < 7)
+            {
+                this.RPT_LIST.Items.Add("Destroyer");
+                this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.DESTROYER);
+            }
+            else
+            {
+                Logger.Error("You Cannot Have More than 7 Ships in a Fleet!");
+            }
+        }
+
+        private void SU_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.advancedOptions.ShipTypes.Count < 7)
+            {
+                this.RPT_LIST.Items.Add("Submarine");
+                this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.SUBMARINE);
+            }
+            else
+            {
+                Logger.Error("You Cannot Have More than 7 Ships in a Fleet!");
+            }
+        }
+
+        private void CR_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.advancedOptions.ShipTypes.Count < 7)
+            {
+                this.RPT_LIST.Items.Add("Cruiser");
+                this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.CRUISER);
+            }
+            else
+            {
+                Logger.Error("You Cannot Have More than 7 Ships in a Fleet!");
+            }
+        }
+
+        private void BA_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.advancedOptions.ShipTypes.Count < 7)
+            {
+                this.RPT_LIST.Items.Add("Battleship");
+                this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.BATTLESHIP);
+            }
+            else
+            {
+                Logger.Error("You Cannot Have More than 7 Ships in a Fleet!");
+            }
+        }
+
+        private void CA_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.advancedOptions.ShipTypes.Count < 7)
+            {
+                this.RPT_LIST.Items.Add("Carrier");
+                this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.CARRIER);
+            }
+            else
+            {
+                Logger.Error("You Cannot Have More than 7 Ships in a Fleet!");
+            }
+        }
+
+        private void RESET_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            this.RPT_LIST.Items.Clear();
+
+            // Create a new AdvancedOptions.ShipTypes class and call the default values.
+            this.advancedOptions.ShipTypes = new List<StatusCodes.ShipType>();
+
+            this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.DESTROYER);
+            this.RPT_LIST.Items.Add("Destroyer");
+
+            this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.SUBMARINE);
+            this.RPT_LIST.Items.Add("Submarine");
+
+            this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.CRUISER);
+            this.RPT_LIST.Items.Add("Cruiser");
+
+            this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.BATTLESHIP);
+            this.RPT_LIST.Items.Add("Battleship");
+
+            this.advancedOptions.ShipTypes.Add(StatusCodes.ShipType.CARRIER);
+            this.RPT_LIST.Items.Add("Carrier");
+        }
+
+        private void SUBMIT_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DELETE_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.RPT_LIST.SelectedIndex == -1)
+            {
+                Logger.Error("You Need To Select Something Before Deleting It!");
+            }
+            else
+            {
+                int index = this.RPT_LIST.SelectedIndex;
+                this.RPT_LIST.Items.RemoveAt(index);
+                this.advancedOptions.ShipTypes.RemoveAt(index);
+            }
+        }
+    }
+}
