3c352c6d7bc9c2e305021a0fe59b8ace4e6ef646
Samuel Mace <samuelcmace@gmail.com>
2022-05-18 13:23:42 -0500

    Fix StyleCop Error

--- Battleship/AboutWindow.xaml.cs
+++ Battleship/AboutWindow.xaml.cs
@@ -3,12 +3,10 @@
 //     MIT License, 2022 (https://mit-license.org).
 // </copyright>
 //-----------------------------------------------------------------------
-
-using System.Windows.Navigation;
-
 namespace Battleship
 {
     using System.Windows;
+    using System.Windows.Navigation;
 
     /// <summary>
     /// Interaction logic for AboutWindow.xaml.
@@ -33,6 +31,11 @@ namespace Battleship
             this.Close();
         }
 
+        /// <summary>
+        /// Opens a hyperlink containing the reference to the license.
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param>
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             System.Diagnostics.Process.Start("https://mit-license.org");
